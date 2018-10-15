99158bb4d892cec3385477993472dbee7f610b23
Eilís Ní Fhlannagáin <pidge@toganlabs.ie>
2018-10-15 12:54:53 +0100

    .config: Update to remove abs pathing
    Removed some old yocto-cruft from .config by just doing
    a normal build.
    
    Signed-off-by: Eilís Ní Fhlannagáin <pidge@toganlabs.ie>

--- .missing-syscalls.d
+++ .missing-syscalls.d
@@ -1,16 +1,10 @@
--:  \
- /home/pidge/e-ale-poky/build/tmp/work-shared/beaglebone-yocto/kernel-source/include/linux/kconfig.h \
- include/generated/autoconf.h \
- /home/pidge/e-ale-poky/build/tmp/work-shared/beaglebone-yocto/kernel-source/include/linux/compiler_types.h \
- /home/pidge/e-ale-poky/build/tmp/work-shared/beaglebone-yocto/kernel-source/include/linux/compiler-gcc.h \
- /home/pidge/e-ale-poky/build/tmp/work-shared/beaglebone-yocto/kernel-source/arch/arm/include/uapi/asm/types.h \
- /home/pidge/e-ale-poky/build/tmp/work-shared/beaglebone-yocto/kernel-source/include/asm-generic/int-ll64.h \
- /home/pidge/e-ale-poky/build/tmp/work-shared/beaglebone-yocto/kernel-source/include/uapi/asm-generic/int-ll64.h \
+-:  include/linux/kconfig.h include/generated/autoconf.h \
+ include/linux/compiler_types.h include/linux/compiler-gcc.h \
+ arch/arm/include/uapi/asm/types.h include/asm-generic/int-ll64.h \
+ include/uapi/asm-generic/int-ll64.h \
  arch/arm/include/generated/uapi/asm/bitsperlong.h \
- /home/pidge/e-ale-poky/build/tmp/work-shared/beaglebone-yocto/kernel-source/include/asm-generic/bitsperlong.h \
- /home/pidge/e-ale-poky/build/tmp/work-shared/beaglebone-yocto/kernel-source/include/uapi/asm-generic/bitsperlong.h \
- /home/pidge/e-ale-poky/build/tmp/work-shared/beaglebone-yocto/kernel-source/arch/arm/include/asm/unistd.h \
- /home/pidge/e-ale-poky/build/tmp/work-shared/beaglebone-yocto/kernel-source/arch/arm/include/uapi/asm/unistd.h \
+ include/asm-generic/bitsperlong.h include/uapi/asm-generic/bitsperlong.h \
+ arch/arm/include/asm/unistd.h arch/arm/include/uapi/asm/unistd.h \
  arch/arm/include/generated/uapi/asm/unistd-eabi.h \
  arch/arm/include/generated/uapi/asm/unistd-common.h \
  arch/arm/include/generated/asm/unistd-nr.h
