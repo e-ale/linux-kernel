b5df0b1347301d03434d2d743e6d23edea95ee2e
Elizabeth 'pidge' Flanagan <pidge@toganlabs.com>
2018-10-15 10:25:41 +0100

    Initial commit
    Initial commit of the kernel and .config
    
    Linux/arm 4.18.9 Kernel
    
    Tested with the arm-poky-linux-gnueabi-gcc (GCC) 8.2.0 toolchain.
    
    Signed-off-by: Elizabeth 'pidge' Flanagan <pidge@toganlabs.com>

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/*  PREEMPT */
+#define UTS_MACHINE "x86_64"
+#define UTS_VERSION "#1 PREEMPT Mon Oct 15 09:59:12 IST 2018"
+#define LINUX_COMPILE_BY "pidge"
+#define LINUX_COMPILE_HOST "build2"
+#define LINUX_COMPILER "gcc version 5.5.0 20171010 (Ubuntu 5.5.0-12ubuntu1~14.04)"
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "4.18.9-yocto-standard"
